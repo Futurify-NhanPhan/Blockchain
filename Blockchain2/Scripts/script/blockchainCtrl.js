a2d3cc846768a3cc202984375d8d5888356987cf
Nhan Phan <nhanphan@futurify.vn>
2017-10-31 16:55:10 +0700

    Get transaction by role

--- Blockchain2/Scripts/script/blockchainCtrl.js
+++ Blockchain2/Scripts/script/blockchainCtrl.js
@@ -11,6 +11,29 @@ myApp.controller('blockchainCtrl', ['$scope', '$http', function ($scope, $http) 
     //}).error(function () {
     //    alert('error');
     //});
+    $scope.roles = [
+        {
+            Id:0, Name:"User"
+        },
+        {
+            Id: 1, Name: "Bank A"
+        },
+        {
+            Id: 2, Name: "Band B"
+        },
+    ];
+    $scope.transaction = {};
+    $scope.checkInfo = function () {
+        if ($scope.role == 0) {
+            $scope.transaction= { TransactionId: "aa1", Data: {Name: "Nhân", MedicalHistory:"ABC", Address: "Viet Nam"}}
+        }
+        if ($scope.role == 1) {
+            $scope.transaction = { TransactionId: "aa1", Data: {Name: "Nhân",  Address: "Viet Nam" }}
+        }
+        if ($scope.role == 2) {
+            $scope.transaction = { TransactionId: "aa1", Data: null }
+        }
+    }
 
 
 }]);
